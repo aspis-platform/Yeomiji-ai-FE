608e883627443cb3c5af8ac7f4e6a74d71825290
Sohee Kim <ttohee1275@gmail.com>
2025-05-09 11:37:34 +0900

    chore: initial settings

---
+++ src/style/theme.tsx
@@ -0,0 +1,14 @@
+export const theme = {
+  color: {
+    black: "#2F2F2F",
+    white: "#FFFFFF",
+    red: "#F33434",
+
+    main: {
+      1: "#D1F8EF",
+      2: "#578FCA",
+      3: "#A1E3F9",
+      4: "#3674B5",
+    },
+  },
+};
